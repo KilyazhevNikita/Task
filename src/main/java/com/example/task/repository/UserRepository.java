fd4154ac0d68b95dd4ec1eb34a73e9ded4ba0a89
KilyazhevNikita <n.kilyazhev@yandex.ru>
2021-10-29 16:00:18 +0300

    update UserRepository

--- src/main/java/com/example/task/repository/UserRepository.java
+++ src/main/java/com/example/task/repository/UserRepository.java
@@ -4,6 +4,15 @@ import com.example.task.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findByOnlineAndTimestampAfter(Boolean online, Timestamp timestamp);
+
+    List<User> findByTimestampAfter(Timestamp timestamp);
+
+    List<User> findByOnline(Boolean online);
 }
